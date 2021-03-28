f319fc5d222f4b7cef18aed58db0cfd32b9b9587
nahmed99 <ahmed_nawaz@hotmail.com>
2021-03-28 15:06:35 +0100

    Video and audio now being shown on webpage...

--- public/script.js
+++ public/script.js
@@ -1,9 +1,40 @@
 // Connect socket to the root path
 const socket = io('/'); 
 
-// Send event to server (join room)
-socket.emit('join-room', ROOM_ID, 10);
+const videoGrid = document.getElementById('video-grid');
+
+// This will allocate id for each new user itself
+const myPeer = new Peer(undefined, {
+   host: '/',
+   port: '3001'
+});
+
+const myVideo = document.createElement('video');
+myVideo.muted = true; // Mute our own video - don't want to listen to ourselves! Others will still hear us.
+
+navigator.mediaDevices.getUserMedia({
+   video: true,
+   audio: true
+}).then(stream => {
+   addVideoStream(myVideo, stream);
+});
+
+// As soon as we connect with peer server and get back 
+// an id, we want to run codebelow
+myPeer.on('open', id => {
+   // Send event to server (join room)
+   socket.emit('join-room', ROOM_ID, id);
+});
 
 socket.on('user-connected', userId => {
-   console.log('User connected' + userId);
+   console.log('User connected: ' + userId);
 }); 
+
+
+function addVideoStream(video, stream) {
+   video.srcObject = stream;
+   video.addEventListener('loadedmetadata', () => {
+      video.play(); // play the video once it is loaded on our screen
+   });
+   videoGrid.append(video);
+}
--- readme.md
+++ readme.md
@@ -5,6 +5,11 @@ Created the project folder (using command line), and ran the following commands 
 - npm i uuid
 - npm i --save-dev nodemon
 
+Also installed the following, to install a peer library to run a peer server:
+
+- npm i -g peer
+
+
 To Run:
 
 As the following script has been added in package.json:
@@ -15,3 +20,7 @@ Type the following in terminal/cmd:
 
 - npm run devStart
 
+
+To run the peer server (use a separate cmd terminal):
+
+- peerjs --port 3001
